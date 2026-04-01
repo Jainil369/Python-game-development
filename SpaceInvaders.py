ab8530b92d93183f11ec63564c4dab6c4738f4cb
Jainil369 <jainilp369@gmail.com>
2026-04-01 17:51:36 +0100

    Add files via upload

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,8 +1,13 @@
 import pygame
+import random
+import time
 
 pygame.init()
 screen = pygame.display.set_mode((400,400))
 background = pygame.image.load("images/space2.png")
+score = 0
+font = pygame.font.SysFont("Arial",30)
+GameOver = False
 
 class Spaceship(pygame.sprite.Sprite):
     def __init__ (self,x,y):
@@ -12,6 +17,7 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect.center = [x,y]
         self.lastshot = pygame.time.get_ticks()
     def move(self):
+        global GameOver
         cooldown = 500
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
@@ -23,6 +29,9 @@ class Spaceship(pygame.sprite.Sprite):
             bullet1 = Bullet(self.rect.centerx,350)
             bullet_group.add(bullet1)
             self.lastshot = current_time
+        if pygame.sprite.spritecollide(self,enemy_group,True):
+            self.kill()
+            GameOver = True
             
 
 class Enemy(pygame.sprite.Sprite):
@@ -33,7 +42,7 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.center = [x,y]
         self.pos = float(self.rect.y)
         self.velocity = 0
-        self.gravity = 0.001
+        self.gravity = 0.0001
     def update(self):
         self.velocity += self.gravity
         self.pos += self.velocity
@@ -46,14 +55,18 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
     def update(self):
+        global score
         self.rect.y -= 3
-        if pygame.sprite.spritecollide(self,enemy_group,True):
+        b =  pygame.sprite.spritecollide(self,enemy_group,True)
+        for i in b:
             self.kill()
+            score += 1
 
+    
 class Enemy_Bullet(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("images/laser.png")
+        self.image = pygame.image.load("images/laser2.png")
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
     def update(self):
@@ -68,6 +81,7 @@ spaceship_group.add(spaceship1)
 enemy_group = pygame.sprite.Group()
 
 bullet_group = pygame.sprite.Group()
+enemybullet_group = pygame.sprite.Group()
 
 def enemies():
     for g in range(3):
@@ -79,17 +93,35 @@ def enemies():
         
 enemies()
 
+lasts = pygame.time.get_ticks()
+
 while True:
     screen.blit(background,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+    if GameOver == True:
+        c = font.render("Game Over",True,"Red")
+        screen.blit(c,(200,200))
+        pygame.display.update()
+        time.sleep(10)
+        break
     enemy_group.draw(screen)   
     enemy_group.update()
     bullet_group.draw(screen)
     bullet_group.update()
     spaceship_group.draw(screen)        
     spaceship1.move()
+    currentt = pygame.time.get_ticks()
+    if currentt - lasts > 1000 and len(enemy_group) > 0:
+        en = random.choice(enemy_group.sprites())
+        enbullet1 = Enemy_Bullet(en.rect.centerx,en.rect.bottom)
+        enemybullet_group.add(enbullet1)  
+        lasts = currentt
+    enemybullet_group.draw(screen)
+    enemybullet_group.update()
+    a = font.render("Score:" +str(score),True,"red")
+    screen.blit(a,(300,50))
     pygame.display.update()
     
     
